03632674246d014a2f1d57c5259e2e23112db3f6
pkar <pkar@ieee.org>
2021-03-28 12:56:27 +0200

    chore: RadioState peer review

--- src/Uno.UWP/Devices/Radios/RadioState.cs
+++ src/Uno.UWP/Devices/Radios/RadioState.cs
@@ -6,17 +6,9 @@ namespace Windows.Devices.Radios
 {
 	public enum RadioState
 	{
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		Unknown,
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		On,
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		Off,
-#endif
-#if __ANDROID__ || __IOS__ || NET461 || __WASM__ || __SKIA__ || __NETSTD_REFERENCE__ || __MACOS__
 		Disabled,
-#endif
 	}
 }
